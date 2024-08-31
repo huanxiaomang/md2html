b30b05a398fbe013223f9a722121321c4dd4559e
huanxiaomang <atri2022@163.com>
2024-08-31 12:27:14 +0800

    feat: done core/ logic

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,28 +4,7 @@ import { markdownToHtml } from "./markdownToHtml";
 export function mdToHTML(markdown: string) {
 
     const html = markdownToHtml(markdown);
-    const markdown1 = `
-    # Main Header
-    
-    **Bold text** and *italic text*.
-    
-    1. First ordered item
-    2. Second ordered item
-    
-    - First unordered item
-    - Second unordered item
-    
-    > A blockquote
-    
-    \`\`\`
-    Code block
-    \`\`\`
-    
-    [Link](http://example.com)
-    ![Image](http://example.com/image.png)
-            `;
-    console.log(markdownToHtml(markdown1));
 
-    return 'sb';
+    return html;
 
 }
